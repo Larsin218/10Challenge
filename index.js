d6bdf59e9b00d61cfca14d3fe85ffdc1c792feae
Luke Smith <larsin.218@gmail.com>
2024-05-14 01:57:32 -0600

    fixed shapes.js

--- index.js
+++ index.js
@@ -1,29 +1,55 @@
-const inquirer = require('inquirer');
-const jest = require('jest');
-const SVG = require('svg');
-const fs = require('fs');
+const inquirer = require("inquirer");
+const fs = require("fs");
+const { Circle, Square, Triangle } = require("./lib/shapes");
 
 const questions = [
-    {
-        type: 'list',
-        name: 'shape',
-        message: 'Pick a shape:',
-        choices: ['Circle', 'Square', 'Triangle']
-    },
-    {
-        type: 'input',
-        name: 'shape-color',
-        message: 'What color will the shape be:'
-    },
-    {
-        type: 'input',
-        name: 'text',
-        message: 'Enter up to 3 characters to put into the shape:'
-    },
-    {
-        type: 'input',
-        name: 'text-color',
-        message: 'What color will the text be:'
-    }
+  {
+    type: "list",
+    name: "shape",
+    message: "Pick a shape:",
+    choices: ["Circle", "Square", "Triangle"],
+  },
+  {
+    type: "input",
+    name: "color",
+    message: "What color will the shape be:",
+  },
+  {
+    type: "input",
+    name: "text",
+    message: "Enter up to 3 characters to put into the shape:",
+  },
+  {
+    type: "input",
+    name: "textColor",
+    message: "What color will the text be:",
+  },
 ];
 
+inquirer
+  .prompt([
+    {
+      type: "list",
+      name: "shape",
+      message: "Pick a shape:",
+      choices: ["Circle", "Square", "Triangle"],
+    },
+    {
+      type: "input",
+      name: "color",
+      message: "What color will the shape be:",
+    },
+    {
+      type: "input",
+      name: "text",
+      message: "Enter up to 3 characters to put into the shape:",
+    },
+    {
+      type: "input",
+      name: "textColor",
+      message: "What color will the text be:",
+    },
+  ])
+  .then((answers) => {
+    // TODO: finish this
+  });
