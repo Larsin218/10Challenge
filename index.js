c66f0da890a7f279af83fd285f5c134dd7c7507b
Luke Smith <larsin.218@gmail.com>
2024-05-15 03:55:18 -0600

    index stuff

--- index.js
+++ index.js
@@ -2,30 +2,6 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 const { Circle, Square, Triangle } = require("./lib/shapes");
 
-const questions = [
-  {
-    type: "list",
-    name: "shape",
-    message: "Pick a shape:",
-    choices: ["Circle", "Square", "Triangle"],
-  },
-  {
-    type: "input",
-    name: "color",
-    message: "What color will the shape be:",
-  },
-  {
-    type: "input",
-    name: "text",
-    message: "Enter up to 3 characters to put into the shape:",
-  },
-  {
-    type: "input",
-    name: "textColor",
-    message: "What color will the text be:",
-  },
-];
-
 inquirer
   .prompt([
     {
@@ -43,6 +19,7 @@ inquirer
       type: "input",
       name: "text",
       message: "Enter up to 3 characters to put into the shape:",
+      validate: input => input.length > 3 ? "Text cannot be longer that 3 characters." : true
     },
     {
       type: "input",
@@ -51,5 +28,5 @@ inquirer
     },
   ])
   .then((answers) => {
-    // TODO: finish this
+    
   });
