4eef788195e67337843cd8b2c98451bffe523761
Luke Smith <larsin.218@gmail.com>
2024-05-15 18:32:05 -0600

    finished first draft

--- index.js
+++ index.js
@@ -2,31 +2,61 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 const { Circle, Square, Triangle } = require("./lib/shapes");
 
-inquirer
-  .prompt([
-    {
-      type: "list",
-      name: "shape",
-      message: "Pick a shape:",
-      choices: ["Circle", "Square", "Triangle"],
-    },
-    {
-      type: "input",
-      name: "color",
-      message: "What color will the shape be:",
-    },
-    {
-      type: "input",
-      name: "text",
-      message: "Enter up to 3 characters to put into the shape:",
-      validate: input => input.length > 3 ? "Text cannot be longer that 3 characters." : true
-    },
-    {
-      type: "input",
-      name: "textColor",
-      message: "What color will the text be:",
-    },
-  ])
-  .then((answers) => {
-    
-  });
+const questions = [
+  {
+    type: "list",
+    name: "shape",
+    message: "Pick a shape:",
+    choices: ["Circle", "Square", "Triangle"],
+  },
+  {
+    type: "input",
+    name: "shapeColor",
+    message: "What color will the shape be:",
+  },
+  {
+    type: "input",
+    name: "text",
+    message: "Enter up to 3 characters to put into the shape:",
+    validate: (input) =>
+      input.length > 3 ? "Text cannot be longer that 3 characters." : true,
+  },
+  {
+    type: "input",
+    name: "textColor",
+    message: "What color will the text be:",
+  },
+];
+
+const makeSVG = (shape, shapeColor, text, textColor) => {
+  const width = 300,
+    height = 200;
+
+  switch (shape) {
+    case "Circle":
+      shapeSVG = Circle;
+      break;
+    case "Square":
+      shapeSVG = Square;
+      break;
+    case "Triangle":
+      shapeSVG = Triangle;
+      break;
+  }
+
+  return `<svg version="1.1" width="${width}" height="${height}" xmlns="http://www.w3.org/2000/svg">
+  ${shapeSVG}
+  <text x="150" y="125" font-size="60" text-anchor="middle" fill="${textColor}">${text}</text>
+  </svg>`;
+};
+
+const saveSVGFile = (SVGLogo) => {
+  fs.writeFileSync('logo.svg', SVGLogo);
+  console.log('Generated logo.svg');
+};
+
+inquirer.prompt(questions).then(answers => {
+  const { shape, shapeColor, text, textColor } = answers;
+  const SVGLogo = makeSVG(shape, shapeColor, text, textColor);
+  saveSVGFile(SVGLogo);
+})
